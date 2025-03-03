3d9ee3cf78b224726639450d893e2f2010922964
thucheh <thucheh_10@hotmail.com>
2025-03-03 12:20:27 +0000

    cadastro das CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,13 +8,50 @@
 
 int main() {
 
-char estado
-char codigoda carta []
-char nome da cidade []
-float area em km²
-float População
-int numero de ponto turisticos
-int Pib
+char Estado[40];
+char CodigoCarta[45];
+char NomeCidade[65];
+int NumeroPopulacao;
+float Areakm2;
+float PIB = 0.0;
+int Pontosturisticos[50]; // { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+//  int PontoTuristico = Pontosturisticos[49];
+
+//  numero_especial A, E, I;
+
+printf("\n\n\n\n\n\n\n", Estado);
+
+printf("Digite o Estado: ");
+scanf("%s", Estado);
+
+printf("Digite o Código da carta: ");
+scanf("%s", CodigoCarta);
+
+printf("Digite o Nome da cidade: ");
+scanf("%s", NomeCidade);
+
+printf("Digite a Area em km2: ");
+scanf("%f", &Areakm2);
+
+printf("Digite o PIB: ");
+scanf("%f", &PIB);    
+
+printf("Digite População: ");
+scanf("%i", &NumeroPopulacao);
+
+printf("Digite o Número de pontos turísticos ");
+scanf("%i", &Pontosturisticos[23]);
+
+
+printf("\n--jogo supertrunfo--\n\n");
+
+printf("Estado: %s\n", Estado);
+printf("Código da carta: %s\n", CodigoCarta);
+printf("Nome da cidade: %s\n", NomeCidade);
+printf("Área em km2: %.2f\n", Areakm2);
+printf("PIB: %.2f\n", PIB);
+printf("População: %i\n", NumeroPopulacao);
+printf("Número de pontos turísticos: %i\n", Pontosturisticos[23]);
 
 
 
